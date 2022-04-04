414953a1a980087a79fbd548cf83bb0a5fff0731
Patryk <patrykreba95@gmail.com>
2022-04-04 21:16:31 +0200

    cvUpdate

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,14 +3,16 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/patryk_reba.pdf";
+import cv from "../../Assets/../Assets/cv.png";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-
+  "https://github.com/patryk-reba/Portfolio/blob/ee874d69b193c2520771d7fe9dc243ec53160638/src/Assets/patryk_reba.pdf";
+// "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+// zamiast tego pdf dam tu po prostu JPG
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -35,9 +37,10 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <img src={cv} style={{ width: "60%" }} />
+          {/* <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          </Document> */}
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
