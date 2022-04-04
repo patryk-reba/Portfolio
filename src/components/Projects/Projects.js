ee874d69b193c2520771d7fe9dc243ec53160638
Patryk <patrykreba95@gmail.com>
2022-04-04 21:02:10 +0200

    CV

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,6 +29,7 @@ function Projects() {
               title="Tenzies Game"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               link="https://tenzies-game-reba.netlify.app/"
+              style={{ borderRadius: "50%" }}
             />
           </Col>
 
